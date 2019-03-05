dbdc27458d00325cdbd9dc5f2627ebf90cae33e1
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-05 12:02:49 +0300

    Add code that runs simple server

--- src/hello_world_heroku/core.clj
+++ src/hello_world_heroku/core.clj
@@ -1,7 +1,17 @@
 (ns hello-world-heroku.core
+  (:require [org.httpkit.server :as s]
+            [environ.core :refer [env]])
   (:gen-class))
 
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello world heroku!"})
+
+(defn start-server [port]
+  (s/run-server app {:port port}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (start-server port)))
