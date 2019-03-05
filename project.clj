78336681796fd09402a002e16ac63a9c8947a505
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-05 11:57:59 +0300

    Add http-kit & environ

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [http-kit "2.3.0"]
+                 [environ "1.1.0"]]
   :main ^:skip-aot hello-world-heroku.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
